5904011faee7b6d53a5c671eab71a96af9a4e0a1
annemusau <moigeanne646@gmail.com>
2024-08-07 11:43:04 +0800

    commiting all

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  //  use of useState
+  const [inCart, setInCart] = useState(false);
+  
+  //set className and button text based on inCart state
+  const cartClass = inCart ? "in-cart" : "";
+  const buttonText = inCart ? "Remove From Cart" : "Add to Cart";
+
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={() => setInCart(!inCart)}>
+        {buttonText}
+      </button>
     </li>
   );
 }
